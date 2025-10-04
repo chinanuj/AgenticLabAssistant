d5e3d0f96cc7ea7a40bdf672ed0404f4e6bbecf4
anuj chincholikar <anuj.chin2gmail.com>
2025-10-04 16:13:00 +0530

    Minor changes 2

--- mas_visualization/simulation.py
+++ mas_visualization/simulation.py
@@ -7,9 +7,9 @@ import sqlalchemy
 from mas_visualization.auth import User 
 from typing import Dict, List, Any, Optional
 from agents import HeadLabAssistantAgent, LabAgent
-from mas_visualization.data_models import Booking 
-from mas_visualization.database import database
-from mas_visualization.models import users, labs, bookings
+from data_models import Booking 
+from database import database
+from models import users, labs, bookings
 
 class MultiAgentTrafficSystem:
     """Main system orchestrating all agents"""
