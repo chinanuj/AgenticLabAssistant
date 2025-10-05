733e3409cc6877ded84d36f434ccc4a56daba99e
anuj chincholikar <anuj.chin2gmail.com>
2025-10-05 13:01:33 +0530

    auth.py changes data time from utc to now()

--- mas_visualization/auth.py
+++ mas_visualization/auth.py
@@ -55,9 +55,9 @@ def verify_password(plain_password, hashed_password):
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
