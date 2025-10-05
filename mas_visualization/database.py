75dc501453df508998ab3f7529679ba4211934bb
anuj chincholikar <anuj.chin2gmail.com>
2025-10-05 12:04:21 +0530

    added database url

--- mas_visualization/database.py
+++ mas_visualization/database.py
@@ -1,10 +1,13 @@
 # database.py
 from databases import Database
 from sqlalchemy import create_engine, MetaData
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Define the database file
 # mas_visualization/database.py
-DATABASE_URL = "sqlite:///./mas_visualization/lab_assistant.db" 
+DATABASE_URL = os.getenv("DATABASE_URL","sqlite:///./mas_visualization/lab_assistant.db")
 
 # Create the core database objects
 database = Database(DATABASE_URL)
